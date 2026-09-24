20fbd7759d5e466380412d9b33d79518b28191b6
zch333 <zchzch333@gmail.com>
2026-09-24 15:54:44 -0400

    in class1

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerBehavior : MonoBehaviour
 {
+    private InputAction upButton;
+    private InputAction downButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        upButton = InputSystem.actions.FindAction("up");
+        downButton = InputSystem.actions.FindAction("down");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPosition = transform.position;
+        if (upButton.IsPressed())
+        {
+            playerPosition.y += 2 * Time.deltaTime;
+            Debug.Log("Go up");
+        }
+        if (downButton.IsPressed())
+        {
+            playerPosition.y -= 2 * Time.deltaTime;
+            Debug.Log("Go down");
+        }
+        transform.position = playerPosition;
     }
 }
